638c9205f981532b6f49a258892270c75de50b97
João Victor S. De Moraes <joao337.jvs@gmail.com>
2021-12-21 14:24:01 -0400

    confrma

---
+++ JavaScript-Curso/Aula 15--Repetições/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+
+
+var c = 0
+
+while(c<5){
+    console.log(`Pedaço ${c}`)//Estrura de repetição com teste lógico no ínicio
+    c++}
+
+do {
+    console.log(`Pedaço ${c}`)//Estrutura de repetição com teste lógico no final
+    c++
+}while(c<3)
+
+
+for(c=0; c<10;c++){
+    console.log(`Pedaço ${c}`)
+}
