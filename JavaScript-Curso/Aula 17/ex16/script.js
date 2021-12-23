ee3212c457bd51b7dcc31ffd2f6db844ea35f7b4
João Victor S. De Moraes <joao337.jvs@gmail.com>
2021-12-22 21:12:41 -0400

    Segundo dia

--- JavaScript-Curso/Aula 17/ex16/script.js
+++ JavaScript-Curso/Aula 17/ex16/script.js
@@ -1,55 +1,35 @@
-    var agora = new Date()
-    var data = agora.getFullYear()
-    var sx = document.getElementsByName("rx")
-    
-
-
-function verifica(){
 
 var res= document.getElementById("res")
-var ano = document.getElementById("ano")
-var idade = data-Number(ano.value);
-var ida = idade.toString();
-var img = document.createElement('img')
-img.setAttribute('id','foto')
 
-//var img = document.getElementById("foto")
+function contar(){
 
+        var ini = Number(document.getElementById('ini').value)
+        var fim = Number(document.getElementById('fim').value)
+        var pass = Number(document.getElementById('passo').value)
 
-if(ano.value.length==0 || ano.value>data){
-    window.alert("Verifique os dados e tente novamente")
-}
-else if(idade<12){
-        if(sx[0].checked){gênero = " um garoto",img.setAttribute('src','garoto.jpg')}
-        else { gênero = " uma garota",img.setAttribute('src','garota.jpg')}
+        //res.innerText = `Inicio ${ini} Fim ${fim} passo ${pass}`
+
         
-res.innerHTML = `<P>Detectamos ${gênero} com ${ida} anos<p>`
-    
-}else if(idade<16){
-        if(sx[0].checked){g = "um",img.src="adom.jpg" }
-        else { g = "uma", img.src="adof.jpg"}
-        
-res.innerHTML=`<p>Detectamos ${g} adolescente com ${ida} anos</p>`
+        var s = ' '
 
-}else if(idade<25){
-        if(sx[0].checked){g = "um", img.src = "jovem.jpg"}
-        else { g = "uma", img.src= "jovef.jpg"}
-
-res.innerHTML= `<p>Detectamos ${g} jovem com ${ida} anos</p>`
-    
-
-}else if(idade<55){
-        if(sx[0].checked){g = "um homem", img.src = "adulto.jpg"}
-        else { g = "uma Mulher",img.src = "adulta.jpg"}
-
-res.innerHTML = `<p>Detectamos ${g} com ${ida} anos</p>`
-
-}else{
-    if(sx[0].checked){g="um senhor", img.src = "senhor.jpg"}
-    else{g="uma senhora", img.src="senhora.jpg"}
-
-res.innerHTML= `<p> Detectamos ${g} com ${ida} anos</p>`
-}
-res.style.textAlign = 'center'
-res.appendChild(img)
+        if (fim==0 ){
+                window.alert("A coluna fim deve ser preenchida")
+        }
+       else if (passo.value==0){
+                window.alert('Passo inválido considerando passo 1')
+                for(c=ini;c<=fim;c++){
+                        ini+=c;
+                        s += c + " "
+                        
+                       
+                }  res.innerText = `${s}`
+        } else {
+       
+                for(c=ini;c<=fim;c+=pass){
+                ini+=c
+                s += c + " "
+                
+               
+                }  res.innerText = `${s}`
+        }
 }
