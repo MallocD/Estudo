242135fa398851f6f30195ab64ccf604de3091ba
João Victor S. De Moraes <joao337.jvs@gmail.com>
2021-12-23 10:03:39 -0400

    Terceiro[

--- JavaScript-Curso/Aula 17/ex16/script.js
+++ JavaScript-Curso/Aula 17/ex16/script.js
@@ -1,35 +1,37 @@
 
-var res= document.getElementById("res")
 
 function contar(){
 
         var ini = Number(document.getElementById('ini').value)
         var fim = Number(document.getElementById('fim').value)
         var pass = Number(document.getElementById('passo').value)
+        var res= document.getElementById("res")
 
         //res.innerText = `Inicio ${ini} Fim ${fim} passo ${pass}`
 
         
         var s = ' '
 
-        if (fim==0 ){
-                window.alert("A coluna fim deve ser preenchida")
+if (fim==0 ){
+  window.alert("A coluna fim deve ser preenchida")//Condição em que se o valor de fim for = 0 o código irá barrar e pedir para que preencha um valor
+      } else {
+          if (pass==0) { 
+                 window.alert('Passo inválido! Considerando passo 1')//Condição aninhada em que se o valor de passo for = 0 , o código automaticamente irá atribuir o valor = 1
+                 pass = 1
+
+                } if(ini> fim){//Condição para decrementação, caso o valor do ínicio da contagem seja maior que o valor do final, assim o contador irá decrementar
+                        for(c=ini;c>=fim;c-=pass){
+                        s += c + "\u{1F449}"
+                        res.innerHTML = `Contagem <strong>Decrescente</strong>:${s}\u{1F6AC}`}
+
+                         } else  {//Condição para incrementação. quando nenhuma das condições a cima forem atendidas irá cair nessa fazendo assim incrementação ao contador
+                                for(c=ini;c<=fim;c+=pass){
+                                s += c + "\u{1F449}"
+                                res.innerHTML = `Contagem <strong> Crescente</strong> :${s}\u{1F6AC}`//Adicionar emojis \u{Código hexadecimal do emoji
+          }
         }
-       else if (passo.value==0){
-                window.alert('Passo inválido considerando passo 1')
-                for(c=ini;c<=fim;c++){
-                        ini+=c;
-                        s += c + " "
-                        
-                       
-                }  res.innerText = `${s}`
-        } else {
-       
-                for(c=ini;c<=fim;c+=pass){
-                ini+=c
-                s += c + " "
-                
-               
-                }  res.innerText = `${s}`
-        }
-}
+      }
+    }
+        
+          
+        
--- JavaScript-Curso/Aula 17/ex17/script.js
+++ JavaScript-Curso/Aula 17/ex17/script.js
@@ -1,55 +1,9 @@
-    var agora = new Date()
-    var data = agora.getFullYear()
-    var sx = document.getElementsByName("rx")
-    
 
+var res = document.getElementById("res")
 
-function verifica(){
+        function calcular(){
+                var num = Number(document.getElementById("num").value)
+                var i = num
+                res.innerHTML = `1x${num}=${2*i}<p>3x${num}=${3*i}`
 
-var res= document.getElementById("res")
-var ano = document.getElementById("ano")
-var idade = data-Number(ano.value);
-var ida = idade.toString();
-var img = document.createElement('img')
-img.setAttribute('id','foto')
-
-//var img = document.getElementById("foto")
-
-
-if(ano.value.length==0 || ano.value>data){
-    window.alert("Verifique os dados e tente novamente")
-}
-else if(idade<12){
-        if(sx[0].checked){gênero = " um garoto",img.setAttribute('src','garoto.jpg')}
-        else { gênero = " uma garota",img.setAttribute('src','garota.jpg')}
-        
-res.innerHTML = `<P>Detectamos ${gênero} com ${ida} anos<p>`
-    
-}else if(idade<16){
-        if(sx[0].checked){g = "um",img.src="adom.jpg" }
-        else { g = "uma", img.src="adof.jpg"}
-        
-res.innerHTML=`<p>Detectamos ${g} adolescente com ${ida} anos</p>`
-
-}else if(idade<25){
-        if(sx[0].checked){g = "um", img.src = "jovem.jpg"}
-        else { g = "uma", img.src= "jovef.jpg"}
-
-res.innerHTML= `<p>Detectamos ${g} jovem com ${ida} anos</p>`
-    
-
-}else if(idade<55){
-        if(sx[0].checked){g = "um homem", img.src = "adulto.jpg"}
-        else { g = "uma Mulher",img.src = "adulta.jpg"}
-
-res.innerHTML = `<p>Detectamos ${g} com ${ida} anos</p>`
-
-}else{
-    if(sx[0].checked){g="um senhor", img.src = "senhor.jpg"}
-    else{g="uma senhora", img.src="senhora.jpg"}
-
-res.innerHTML= `<p> Detectamos ${g} com ${ida} anos</p>`
-}
-res.style.textAlign = 'center'
-res.appendChild(img)
-}
+        }
