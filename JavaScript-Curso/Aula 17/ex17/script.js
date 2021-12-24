1db12deaa4ff249119337bfaf2dfd4ce36c52a46
João Victor S. De Moraes <joao337.jvs@gmail.com>
2021-12-24 08:48:50 -0400

    4

--- JavaScript-Curso/Aula 17/ex17/script.js
+++
@@ -1,9 +0,0 @@
-
-var res = document.getElementById("res")
-
-        function calcular(){
-                var num = Number(document.getElementById("num").value)
-                var i = num
-                res.innerHTML = `1x${num}=${2*i}<p>3x${num}=${3*i}`
-
-        }
