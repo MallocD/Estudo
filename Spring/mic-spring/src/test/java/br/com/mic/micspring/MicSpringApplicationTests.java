4fa54a5731f6d4756032a7bb4e8860f05d448e8e
João Victor S. De Moraes <joao337.jvs@gmail.com>
2022-01-19 14:32:28 -0400

    IntroduçãoSpring

---
+++ Spring/mic-spring/src/test/java/br/com/mic/micspring/MicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mic.micspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
